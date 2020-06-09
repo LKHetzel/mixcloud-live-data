1bcb6280948ce6e549e6355b5e8af3e43b93c588
LKHetzel <lkhetzel@gmail.com>
2020-06-08 22:02:52 -0500

    working followers code

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // mixcloud-get-data
 // Does API calls to Mixcloud to get data to display on your stream.
 
-var http = require("http");
+const { USERNAME, FILE_DIR, FOLLOWER_FILE, API_URL } = require("./config");
+const axios = require('axios');
+var fs = require('fs');
 
-var options = {
-  host: 'https://api.mixcloud.com/',
-  port: 80,
-  path: '/upload',
-  method: 'POST'
-};
+function getRecentFollower() {
+axios.get(`${API_URL}${USERNAME}/followers/?limit=1`)
+  .then(response => {
+    data = (JSON.parse(JSON.stringify(response.data)));
+    name = (data.data[0].name);
+    console.log("Most Recent Follower is: " + name);
+    fs.writeFile( FILE_DIR + FOLLOWER_FILE, name, function (err) {
+      if (err) throw err;
+      console.log('Wrote ' + name + ' to file.');
+    });
+})};
+
+
+getRecentFollower();
